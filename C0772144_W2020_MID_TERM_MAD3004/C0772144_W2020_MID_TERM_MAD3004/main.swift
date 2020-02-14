033d35c61d30e2ba31c80ae1f40d173979027a5c
C0772144-komalsubhra <komalsubhra6768@gmail.com>
2020-02-13 21:15:58 -0500

    initialization

--- C0772144_W2020_MID_TERM_MAD3004/C0772144_W2020_MID_TERM_MAD3004/main.swift
+++ C0772144_W2020_MID_TERM_MAD3004/C0772144_W2020_MID_TERM_MAD3004/main.swift
@@ -8,4 +8,10 @@
 
 import Foundation
 
+ var c1 = Customer(customerId: 1, customerFirstName: "komal", customerLastName: "subhra", customerFullName: "komal subhra", customerEmailAddress: "komalcuty40@gmail.com", ArrayOfBills: 23, totalBillToPay: 56000)
+var c2 = Customer(customerId: 1, customerFirstName: "vivek", customerLastName: "subhra", customerFullName: "vivek subhra", customerEmailAddress: "vivek123@gmail.com", ArrayOfBills: 23, totalBillToPay: 46000)
+c1.display()
+c2.display()
+
+
 
