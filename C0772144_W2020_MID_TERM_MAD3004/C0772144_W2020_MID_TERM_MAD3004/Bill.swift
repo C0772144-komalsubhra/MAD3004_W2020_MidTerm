ff0c7ce073f1ca09e764a6ce52d0e2b860884bf5
C0772144-komalsubhra <komalsubhra6768@gmail.com>
2020-02-13 20:46:39 -0500

    Creating Bill Class

---
+++ C0772144_W2020_MID_TERM_MAD3004/C0772144_W2020_MID_TERM_MAD3004/Bill.swift
@@ -0,0 +1,29 @@
+//
+//  Bill.swift
+//  C0772144_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Bill: IDisplay{
+   
+    func display() {
+        <#code#>
+    }
+    
+    var billId: Int
+    var billDate: Date
+    var billType: String
+    var totalBillAmmount: Double
+    
+    init(billId: Int,billDate: Date,billType: String,totalBillAmmount: Double){
+        self.billId = billId
+        self.billDate = billDate
+        self.billType = billType
+        self.totalBillAmmount = totalBillAmmount
+    }
+    
+}
+
