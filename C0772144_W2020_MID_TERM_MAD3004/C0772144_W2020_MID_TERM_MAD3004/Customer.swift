73b132bbd19abfe5ba3b94124b5cef934c532020
C0772144-komalsubhra <komalsubhra6768@gmail.com>
2020-02-18 18:08:49 -0500

    init function main class

--- C0772144_W2020_MID_TERM_MAD3004/C0772144_W2020_MID_TERM_MAD3004/Customer.swift
+++ C0772144_W2020_MID_TERM_MAD3004/C0772144_W2020_MID_TERM_MAD3004/Customer.swift
@@ -20,8 +20,25 @@ var emailAddress:String?
    
     var totalBillToPay:Double=0.00
     
+init(customerId:Int,firstName:String,lastName:String,emailAddress:String)
+{
+self.customerId=customerId
+self.firstName=firstName
+self.lastName=lastName
+    
+    if emailValidation(email: emailAddress)
+    {
+        self.emailAddress=emailAddress
+    }
+     else
+     {
+         print("Invalid Email ID for \(customerId) : \(emailAddress) ")
+     }
 
 
+}
+
+ 
 }
    
  
